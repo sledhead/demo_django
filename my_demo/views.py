295156616779d734ac58f9f6586ea21596a99df1
sledhead <bigskyprogrammers@gmail.com>
2023-02-09 01:46:00 +0000

    home page testing

--- my_demo/views.py
+++ my_demo/views.py
@@ -1,4 +1,5 @@
 
+import random
 from django.http import HttpResponse
 
 HTML_STRING = """
@@ -6,4 +7,7 @@ HTML_STRING = """
 <h2>Montana Big Sky Country</h2>
 """
 def home_view(request):
-    return HttpResponse(HTML_STRING)
+
+    random_int = random.randint(10, 500010)
+    new_return_str = f'{HTML_STRING} New Number: {random_int}'
+    return HttpResponse(new_return_str)
