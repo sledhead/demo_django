29689bbff77602e4c21c4a8160c08b65197d6da8
sledhead <bigskyprogrammers@gmail.com>
2023-02-09 05:10:54 +0000

    html render

--- my_demo/views.py
+++ my_demo/views.py
@@ -1,6 +1,9 @@
 
 import random
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+from articles.models import Article
 
 HTML_STRING = """
 <h1>Hello World</h1>
@@ -9,5 +12,19 @@ HTML_STRING = """
 def home_view(request):
 
     random_int = random.randint(10, 500010)
-    new_return_str = f'{HTML_STRING} New Number: {random_int}'
+    database_rec = Article.objects.get(id=2)
+
+    database_rec_dict = {
+
+        'title': database_rec.title,
+        'content': database_rec.content,
+        'id': database_rec.id
+    }
+
+    #new_database_str = f'<b>Database info:</b> {database_rec.title}, content: {database_rec.content}'
+    #new_database_str = '<b>Database info:</b> {title}, content: {content}'.format(**database_rec_dict)
+
+    new_database_str = render_to_string('home-view.html', context = database_rec_dict)
+    new_return_str = f'{HTML_STRING} New Number: {random_int}<br>'
+    new_return_str += new_database_str
     return HttpResponse(new_return_str)
