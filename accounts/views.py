4dd1c7677b5a65afadd97352e4cfe76399096fb3
sledhead <bigskyprogrammers@gmail.com>
2023-02-24 04:38:43 +0000

    login new code

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -16,19 +16,13 @@ def register_view(request):
 def login_view(request):
 
     if( request.method == "POST"):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        #print(username,password)
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is None:
-            context = {"error":"Invalid username or password"}
-            return render(request,"accounts/login.html",context)
-        
-        #print(user)
-        login(request,user)
-        return redirect('/')
+        form = AuthenticationForm(request, data= request.POST)
+        if( form.is_valid() ):
+            user = form.get_user()
+            login(request,user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
 
     return render(request,"accounts/login.html",{})
 
