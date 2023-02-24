51ce1583cde3ff30eb2d0a27ce36025bc936dc46
sledhead <bigskyprogrammers@gmail.com>
2023-02-24 03:21:48 +0000

    cleaning user data

--- articles/forms.py
+++ articles/forms.py
@@ -7,7 +7,16 @@ class ArticleForm(forms.ModelForm):
         model = Article
         fields = ['title', 'content']
 
-        
+
+    def clean(self):
+        data = self.cleaned_data
+        title = data.get('title')
+        qs = Article.objects.all().filter(title__icontains=title)
+
+        if( qs.exists() ):
+            self.add_error("title", f"{title} is already in use. Please pick another title.")
+
+        return data
 
 class ArticleFormOld(forms.Form):
     title = forms.CharField()
