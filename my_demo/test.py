ba0b01b57771abc55163e657bdfd9904402c9d8e
sledhead <bigskyprogrammers@gmail.com>
2023-03-03 07:30:27 +0000

    basic testing code

--- my_demo/test.py
+++ my_demo/test.py
@@ -1,9 +1,17 @@
 import os
 from django.conf import settings
+from django.contrib.auth.password_validation import validate_password
 from django.test import TestCase
 
 class TryDjangoConfigTest( TestCase ):
 
     def test_secret_key_strength(self):
         SECRET_KEY = os.environ.get('SECRET_KEY')
-        self.assertNotEqual(SECRET_KEY,'abc1223')
+        #self.assertNotEqual(SECRET_KEY,'abc122')
+
+        try:
+            is_strong = validate_password(SECRET_KEY)
+
+        except Exception as e:
+            msg = f'Bad Secret Key {e.messages}'
+            self.fail(msg)
