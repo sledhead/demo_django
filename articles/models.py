440fd2e1a48163791e0be5d5c12e18622e72586e
sledhead <bigskyprogrammers@gmail.com>
2023-03-08 08:05:47 +0000

    adding slug

--- articles/models.py
+++ articles/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField( blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
+
+
+    def save(self, *args, **kwargs):
+
+        if self.slug is None:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
