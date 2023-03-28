03216acd9e0a086e6cc5be8a99b68a64025dd56f
sledhead <bigskyprogrammers@gmail.com>
2023-03-28 05:29:54 +0000

    more user code

--- articles/models.py
+++ articles/models.py
@@ -26,6 +26,7 @@ class ArticleManager(models.Manager):
 
 # Create your models here.
 class Article(models.Model):
+    user = models.ForeignKey("auth.User", blank=True, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField( unique=True, blank=True, null=True)
     content = models.TextField()
