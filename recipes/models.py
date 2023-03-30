713e2a981f902d1d1a6b14c562999b2c965fb37c
sledhead <bigskyprogrammers@gmail.com>
2023-03-30 04:57:26 +0000

    more recipes code

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+class RecipeIngredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    desciption = models.TextField(blank=True, null=True)
+    quanity = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+    
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
