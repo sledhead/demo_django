6927f306d91bdbc1afed7067299422bed702e2e7
sledhead <bigskyprogrammers@gmail.com>
2023-04-04 05:14:29 +0000

    recipes testing code

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    def SetUp(self):
+        self.user_a = User.objects.create_user("codespaces", password='testme')
